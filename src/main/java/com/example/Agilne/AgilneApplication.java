44795b5f1f9f5103d4bced3ca7d05b8148685b84
5ASAKOV <sasa_kov@hotmail.com>
2021-05-08 21:14:26 +0200

    Agilne projekat

---
+++ src/main/java/com/example/Agilne/AgilneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Agilne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilneApplication.class, args);
+	}
+
+}
